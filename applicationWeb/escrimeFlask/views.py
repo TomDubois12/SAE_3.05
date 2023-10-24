24617a09440f8de03bdbb9f09e2dbc3cceb85915
Korentin Georget <korentin.georget@gmail.com>
2023-10-24 16:50:48 +0200

    ajout headers

--- applicationWeb/escrimeFlask/views.py
+++ applicationWeb/escrimeFlask/views.py
@@ -3,5 +3,5 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html',
-                           title='Escrime')
+    return render_template('acceuil.html',
+                           title='Accueil')
