5426b9c94e9e6253cc977079a06937ea96e97110
nathanboissay <nathan.boissay@etu.univ-orleans.fr>
2023-10-24 15:27:27 +0200

    Ajout des fichier pour la base de données

---
+++ applicationWeb/escrimeFlask/delEscrimeDB.sql
@@ -0,0 +1,19 @@
+drop table if exists TIREUR_DANS_COMPETITIONS;
+drop table if exists TIREUR_DANS_POULE;
+drop table if exists ARBITRE_DANS_COMPETITIONS;
+drop table if exists ARBITRE_POULE;
+drop table if exists ARBITRE_ELIMINATION;
+drop table if exists MATCHPOULE;
+drop table if exists POULE;
+drop table if exists MATCHELIMINATION; 
+drop table if exists ORGANISATEURDANSCLUB;
+drop table if exists ARBITRE;
+drop table if exists TIREUR;
+drop table if exists ORGANISATEUR;
+drop table if exists CLUB;
+drop table if exists COMPETITION;
+drop table if exists ARME;
+drop table if exists SEXE;
+drop table if exists CATEGORIE;
+drop table if exists LIEU;
+
