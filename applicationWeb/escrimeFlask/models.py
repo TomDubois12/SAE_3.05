d595ef1f21fe1d8e4e0f4902dda3d56eb15f8fc1
nathanBoissay <144656105+nathanBoissay@users.noreply.github.com>
2023-10-26 11:48:07 +0200

    Update models.py

--- applicationWeb/escrimeFlask/models.py
+++ applicationWeb/escrimeFlask/models.py
@@ -1,8 +1,7 @@
-# import csv
+import csv
 
-# # pip install Flask-MySQLdb
-# #sudo apt-get install python3-dev default-libmysqlclient-dev build-essential pkg-config
-
+# pip install Flask-MySQLdb
+#sudo apt-get install python3-dev default-libmysqlclient-dev build-essential pkg-config
 
 
 import os.path
@@ -18,8 +17,8 @@ db = mysql.connector.connect(
   database = "Escrime"
 )
 
-# #créer un curseur de base de données pour effectuer des opérations SQL
-# # cursor = db.cursor()
+#créer un curseur de base de données pour effectuer des opérations SQL
+cursor = db.cursor()
 
 def insertTireurCompetition(nom : str, prenom : str,numeroLicence : int ,classement : float, idSexe : int, dateNaissanceTireur : str, nation : str, comiteRegional : str, idCompetition: int) -> None:
     try :
@@ -36,77 +35,104 @@ def insertTireurCompetition(nom : str, prenom : str,numeroLicence : int ,classem
     except Exception as mysql_error:
       print(mysql_error)
 
+def insertParticipantByLicence(numeroLicence : int) : 
+   if estDansBDNational(numeroLicence) : 
+      try :
+        infoTireurGlobal = getInfoFromBDNational(numeroLicence)
+        infoTireur = infoTireurGlobal[1]
+        idSexe = infoTireurGlobal[0]
+        if "Dames" in idSexe : 
+           idSexe = 1
+        else : 
+           idSexe = 0
+        requete1 = "insert into TIREUR(nomTireur,prenomTireur,numeroLicenceTireur,classement,idSexeTireur,dateNaissanceTireur,nationTireur,comiteRegionalTireur) values(%s,%s,%s,%s,%s,%s,%s,%s);"
+        cursor.execute(requete1, (infoTireur[0],infoTireur[1],numeroLicence,infoTireur[7],idSexe[0],infoTireur[2],infoTireur[4],infoTireur[5]))
+        db.commit()
+      except Exception as mysql_error:
+        print(mysql_error)
+
+def getInfoFromBDNational(numeroLicence : int) -> list :
+  
+  fichiers = fichiersDossier("./escrimeFlask/csvEscrimeur/")
+  for f in fichiers :
+    infoFichier = classementFile("./escrimeFlask/csvEscrimeur/" + f)
+    for cat in infoFichier :
+       if str(numeroLicence) == cat[3] :
+          return [f,cat]
+  
+
 def estDansBDNational(numeroLicence : int) -> bool:
   res = False
-  fichiers = fichiersDossier("./csvEscrimeur/")
+  fichiers = fichiersDossier("./escrimeFlask/csvEscrimeur/")
   for f in fichiers :
-    infoFichier = classementFile("./csvEscrimeur/" + f)
+    infoFichier = classementFile("./escrimeFlask/csvEscrimeur/" + f)
     for cat in infoFichier :
        if str(numeroLicence) == cat[3] :
           return True
   return res 
 
-# def concourtInscritLicence(numeroLicence : int) -> list:
-#   requete1 = "select * from TIREUR_DANS_COMPETITIONS natural join COMPETITION where numeroLicenceTireur = " + str(numeroLicence) + ";"
-#   cursor.execute(requete1)
-#   info = cursor.fetchall()
-#   res = []
-#   for i in range(len(info)):
-#     requete2 = """select intituleCompet,typeArme, intituleSexe,intituleCategorie, departement
-#                   from COMPETITION natural join LIEU natural join ARME natural join SEXE natural join CATEGORIE
-#                   where datediff(dateDebutCompetiton ,CURDATE()) > 14 and idLieu ="""+ str(info[i][7]) +" and idCategorie ="+ str(info[i][8]) +" and idSexe = "+str(info[i][9]) +" and idArme = "+ str(info[i][10]) +" and idCompetition = "+str(info[i][0]) +";"
-#     cursor.execute(requete2) 
-#     res.append(cursor.fetchall())
-#   # return sous la forme : nomCompetition intituleCompet typeArme intituleSexe intituleCategorie departement
-#   return res
+def concourtInscritLicence(numeroLicence : int) -> list:
+  requete1 = "select * from TIREUR_DANS_COMPETITIONS natural join COMPETITION where numeroLicenceTireur = " + str(numeroLicence) + ";"
+  cursor.execute(requete1)
+  info = cursor.fetchall()
+  res = []
+  for i in range(len(info)):
+    requete2 = """select intituleCompet,typeArme, intituleSexe,intituleCategorie, departement
+                  from COMPETITION natural join LIEU natural join ARME natural join SEXE natural join CATEGORIE
+                  where datediff(dateDebutCompetiton ,CURDATE()) > 14 and idLieu ="""+ str(info[i][7]) +" and idCategorie ="+ str(info[i][8]) +" and idSexe = "+str(info[i][9]) +" and idArme = "+ str(info[i][10]) +" and idCompetition = "+str(info[i][0]) +";"
+    cursor.execute(requete2) 
+    res.append(cursor.fetchall())
+  # return sous la forme : nomCompetition intituleCompet typeArme intituleSexe intituleCategorie departement
+  return res
 
 
 
-# def classementFile(filename :str) -> list:
-#     "nom prenom date_naissance adherent nation comite_regional club points rang"
-#     res = []
-#     with open(filename, 'r',encoding='Latin-1') as file :
-#       next(file)
-#       for ligne in file:
-#         ligne = ligne.split(";")
-#         ligne[8] = ligne[8].replace("\n","")
-#         res.append(ligne)
-#       file.close()
-#     return res
+def classementFile(filename :str) -> list:
+    "nom prenom date_naissance adherent nation comite_regional club points rang"
+    res = []
+    with open(filename, 'r',encoding='Latin-1') as file :
+      next(file)
+      for ligne in file:
+        ligne = ligne.split(";")
+        ligne[8] = ligne[8].replace("\n","")
+        res.append(ligne)
+      file.close()
+    return res
 
 
-# def inscriptionOuverte() -> list:
-#     requete1 = "select * from COMPETITION where datediff(dateDebutCompetiton, CURDATE()) > 14;"
-#     cursor.execute(requete1)
-#     info = cursor.fetchall()
-#     res = []
-#     for i in range(len(info)):
+def inscriptionOuverte() -> list:
+    requete1 = "select * from COMPETITION where datediff(dateDebutCompetiton, CURDATE()) > 14;"
+    cursor.execute(requete1)
+    info = cursor.fetchall()
+    res = []
+    for i in range(len(info)):
       
-#       requete2 = """select intituleCompet,typeArme, intituleSexe,intituleCategorie, departement, idCompetition
-#                     from COMPETITION natural join LIEU natural join ARME natural join SEXE natural join CATEGORIE
-#                     where datediff(dateDebutCompetiton ,CURDATE()) > 14 and idLieu ="""+ str(info[i][6]) +" and idCategorie ="+ str(info[i][7]) +" and idSexe = "+str(info[i][8]) +" and idArme = "+ str(info[i][9]) +" and idCompetition = "+str(info[i][0]) +";"
-#       cursor.execute(requete2) 
-#       res.append(cursor.fetchall())
-#                             # print(info[i][0]) #idCompetition
-#                             # print(info[i][1]) #NomCompetition
-#                             # print(info[i][2]) #saison
-#                             # print(info[i][3]) #estFinit
-#                             # print(info[i][4]) #coeficient
-#                             # print(info[i][5]) #dateDebutCompetiton
-#                             # print(info[i][6]) #idLieu
-#                             # print(info[i][7]) #idCategorie
-#                             # print(info[i][8]) #idSexe
-#                             # print(info[i][9]) #idArme
-#     return res
+      requete2 = """select intituleCompet,typeArme, intituleSexe,intituleCategorie, departement, idCompetition
+                    from COMPETITION natural join LIEU natural join ARME natural join SEXE natural join CATEGORIE
+                    where datediff(dateDebutCompetiton ,CURDATE()) > 14 and idLieu ="""+ str(info[i][6]) +" and idCategorie ="+ str(info[i][7]) +" and idSexe = "+str(info[i][8]) +" and idArme = "+ str(info[i][9]) +" and idCompetition = "+str(info[i][0]) +";"
+      cursor.execute(requete2) 
+      res.append(cursor.fetchall())
+                            # print(info[i][0]) #idCompetition
+                            # print(info[i][1]) #NomCompetition
+                            # print(info[i][2]) #saison
+                            # print(info[i][3]) #estFinit
+                            # print(info[i][4]) #coeficient
+                            # print(info[i][5]) #dateDebutCompetiton
+                            # print(info[i][6]) #idLieu
+                            # print(info[i][7]) #idCategorie
+                            # print(info[i][8]) #idSexe
+                            # print(info[i][9]) #idArme
+    return res
 
-# def getOrganisateurClub():
-#   requete1 = "select * from ORGANISATEURDANSCLUB natural join CLUB ;"
-#   cursor.execute(requete1)
-#   info = cursor.fetchall()
-#   res = dict()
-#   for i in range(len(info)):
-#     res[info[i][1]] = info[i][2]
-#   return res
+def getOrganisateurClub():
+  requete1 = "select * from ORGANISATEURDANSCLUB natural join CLUB ;"
+  cursor.execute(requete1)
+  info = cursor.fetchall()
+  res = dict()
+  for i in range(len(info)):
+    res[info[i][1]] = info[i][2]
+  return res
+
 
 def getProfil(numeroLicence : int) -> list:
   requete1 = """select nomTireur, prenomTireur, dateNaissanceTireur, numeroLicenceTireur,  nationTireur, comiteRegionalTireur,nomCLub
@@ -115,6 +141,7 @@ def getProfil(numeroLicence : int) -> list:
   res = []
   res.append(cursor.fetchone())
   return res
+
 
 def fichiersDossier(path : str) :
   files = os.listdir(path)
@@ -132,4 +159,6 @@ if __name__ == "__main__":
     #print(getProfil(315486))
     #print(estDansBDNational(521531))
     #print(estDansBDNational(138932))
+    #insertParticipantByLicence(138932)
     pass
+
