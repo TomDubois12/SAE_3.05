4d8ad7db9f9a529147d2bfe57864f514969c8105
Robloblo <108494251+Robloblo@users.noreply.github.com>
2024-02-20 11:28:15 +0100

    page d'inscription en solo ou en equipe

--- applicationWeb/escrimeFlask/views.py
+++ applicationWeb/escrimeFlask/views.py
@@ -25,6 +25,12 @@ def inscription():
                            title='Inscription',
                            competitions=inscriptionOuverte())
 
+@app.route('/inscriptionSoloEquipe')
+def inscriptionSoloEquipe():
+    return render_template('inscriptionSoloEquipe.html',
+                           title='Inscription',
+                           competitions=inscriptionOuverte())
+
 @app.route('/connexion_organisateur')
 def connexion_organisateur():
     return render_template('connexion_organisateur.html',
